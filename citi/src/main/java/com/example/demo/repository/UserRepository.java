305c0fa147a0f7123a4fe96bbdd1ef5325f88416
NemaniEsha <76994003+NemaniEsha@users.noreply.github.com>
2021-02-17 14:40:39 +0530

    Update UserRepository.java

--- citi/src/main/java/com/example/demo/repository/UserRepository.java
+++ citi/src/main/java/com/example/demo/repository/UserRepository.java
@@ -10,8 +10,4 @@ public interface UserRepository extends JpaRepository<User,Long>{
 	//User findById(long id);
 	@Query(value = "Select u from User u where email=:email")
 	User findByEmail(String email);
-
-	//Mockito ---https://github.com/sanjaydegaonkar/IRP/tree/master/TDD_Mockito/BankApplication
-	//https://github.com/sanjaydegaonkar/SpringBoot/tree/master/Project_Demos/Employee_CRUD_Query/src/main/java/com/crescent
-	//User-Role mapping ---  https://dzone.com/articles/introduction-to-spring-data-jpa-part-4-bidirection
 }
